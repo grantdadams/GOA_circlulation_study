800a143fca9ec48d17c52ce7fe4ca23f452c0f38
Grant.Adams <grantadams60091@gmail.com>
2026-06-10 09:46:21 -0700

    Update Questions.R

--- Questions.R
+++ Questions.R
@@ -1,6 +1,9 @@
 Questions
 
-# Timing for winter (Dec year, or Jan/Feb year)
-# Indices for ATF
-
 # TODO
+Update rockfish to age-2 minage
+Estimate time-and-age varying M
+Include ecofoci data?
+Grant works on convergence
+Does GOADI/NGAO explain recruitment (base vs direct)
+Which path does GOADI/NGAO explain recruitment (3 sub DSEM models)
